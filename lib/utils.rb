9812d31d49640e07ff4cefd7e8b66908c5c43614
Sathiyan Sivathas <sathiyan.sivathas@metaswitch.com>
2016-11-18 18:36:15 +0000

    Code review markups

--- lib/utils.rb
+++ lib/utils.rb
@@ -33,25 +33,22 @@ def Utils.paramhash_to_str params
 end
 
 def Utils.check_route_matches_destination route, destination
-  regexp = /<sip:(.*?)@(.*?):(.*?);(.*?)>/i
-  dest_address = destination.instance_variable_get(:@ip)
-  dest_port = destination.instance_variable_get(:@port)
+  # Get the address and port out of the destination (note that destination.ip can be a hostname).
+  dest_address = destination.ip
+  dest_port = destination.port
+  regexp = /<sip:(?:.*?@)?(.*?):(.*?);.*?>/i
   if route =~ regexp
-    route_address = $2
-    route_port = $3
+    route_address = $1
+    route_port = $2
   end
 
-  route_ip = []
-  Resolv.each_address(route_address) do |address|
-    route_ip << address
-  end
+  # Get IPs from hostnames.
+  route_ips = Resolv.getaddresses(route_address)
+  dest_ips = Resolv.getaddresses(dest_address)
 
-  dest_ip = []
-  Resolv.each_address(dest_address) do |address|
-    dest_ip << address
-  end
-
-  if (route_ip & dest_ip).empty?
+  # Check that there is at least one address common between the route and the destination,
+  # and that the ports are the same.
+  if (route_ips & dest_ips).empty?
     raise "Error: Address in top route header '#{route_address}' does not match destination address '#{dest_address}'"
   elsif (route_port.to_s != dest_port.to_s)
     raise "Error: Port in top route header '#{route_port}' does not match destination port '#{dest_port}'"
